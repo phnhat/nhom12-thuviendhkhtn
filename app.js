900861255571b737019c11c137edf0a9a2220038
providence97 <ptphuong@example.com>
2019-01-05 23:46:25 +0700

    final file

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var hbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var searchRouter = require('./routes/search');
 var searchttRouter = require('./routes/searchtt');
+var infoRouter = require('./routes/info');
+var orderRouter = require('./routes/order');
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/search', searchRouter);
 app.use('/searchtt', searchttRouter);
+app.use('/info', infoRouter);
+app.use('/order', orderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/search.js
+++ routes/search.js
@@ -2,12 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   var params = req.query;
-  res.render('search', {
-     type: params.t, 
-     query: params.q 
+  if (params.q == "toan cao cap" || params.q == "toan" || params.q == "cao cap" || params.q == "toan cao" || params.q == "cap" || params.q === "toan cap") {
+    res.render('search', {
+      t: params.t,
+      q: params.q,
+      number: "3",
+      condition: true
     });
+  } else {
+    
+    res.render('search', {
+      t: params.t,
+      q: params.q,
+      number: "0",
+      condition: false
+    });
+  }
 });
 
 module.exports = router;
